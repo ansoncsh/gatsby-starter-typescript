b976b4d53f51c7acfed61abe98ad454636aa4d3f
Anson Chow <anson.chowsh@gmail.com>
2020-07-20 10:36:01 +0800

    handle typescript warnings

--- common/wrapRoot.tsx
+++ common/wrapRoot.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from '../src/Layout'
 
-export default ({ element }) => {
-  return <Layout>{element}</Layout>
+export default (props:any) => {
+  return <Layout>{props.element}</Layout>
 }
